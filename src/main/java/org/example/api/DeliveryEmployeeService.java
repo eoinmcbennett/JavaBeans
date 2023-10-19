2b7e64c355330e7b42b1f28490d378f0e8dba2cb
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-10-19 11:33:08 +0100

    Added some comments

--- src/main/java/org/example/api/DeliveryEmployeeService.java
+++ src/main/java/org/example/api/DeliveryEmployeeService.java
@@ -11,6 +11,12 @@ public class DeliveryEmployeeService {
         this.dao = dao;
     }
 
+    /**
+     * Attempts to create a delivery employee using the service data access object
+     * @param deliveryEmployee
+     * @return the created delivery employee id
+     * @throws FailedToCreateException
+     */
     public int createDeliveryEmployee(DeliveryEmployee deliveryEmployee) throws FailedToCreateException {
         return dao.createDeliveryEmployee(deliveryEmployee);
     }
