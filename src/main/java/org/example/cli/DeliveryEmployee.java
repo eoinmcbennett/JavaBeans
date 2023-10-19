0b88eaac4103b200533338959ee138a6a4b1fb34
Mia Green <miagreen14@googlemail.com>
2023-10-19 11:10:34 +0100

    Created DeliveryEmployee class

---
+++ src/main/java/org/example/cli/DeliveryEmployee.java
@@ -0,0 +1,9 @@
+package org.example.cli;
+
+public class DeliveryEmployee extends Employee {
+
+    public DeliveryEmployee(int id, String first_name, String last_name, double salary, String bank_account_number, String ni_number) {
+        super(id, first_name, last_name, salary, bank_account_number, ni_number);
+    }
+
+}
