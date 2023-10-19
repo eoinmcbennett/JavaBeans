be4ca0121fcec56766cf5d969d0cb7e27aace5c5
Mia Green <miagreen14@googlemail.com>
2023-10-19 11:26:48 +0100

    DeliveryEmployeeService created

--- src/main/java/org/example/resources/DeliveryEmployeeController.java
+++ src/main/java/org/example/resources/DeliveryEmployeeController.java
@@ -1,8 +1,12 @@
 package org.example.resources;
 
+import org.example.api.DeliveryEmployeeService;
+import org.example.cli.DeliveryEmployee;
+import org.example.client.FailedToCreateException;
 import org.example.db.DeliveryEmployeeDAO;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -11,11 +15,25 @@ import javax.ws.rs.core.Response;
 @Path("/api")
 public class DeliveryEmployeeController {
 
+    DeliveryEmployeeService deliveryEmployeeService = new DeliveryEmployeeService();
+
     @GET
-    @Path("/deliveryemployees")
+    @Path("/deliveryemployee")
     @Produces(MediaType.APPLICATION_JSON)
     public Response testDAO(){
         DeliveryEmployeeDAO employeeDAO = new DeliveryEmployeeDAO();
         return Response.ok().entity(employeeDAO.getDeliveryEmployeesIds()).build();
+    }
+
+    @POST
+    @Path("/deliveryemployee")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createDeliveryEmployee(DeliveryEmployee deliveryEmployee) {
+        try {
+            return Response.ok(deliveryEmployeeService.createDeliveryEmployee(deliveryEmployee)).build();
+        } catch (FailedToCreateException e) {
+            System.err.println(e.getMessage());
+            return Response.serverError().entity(e.getMessage()).build();
+        }
     }
 }
