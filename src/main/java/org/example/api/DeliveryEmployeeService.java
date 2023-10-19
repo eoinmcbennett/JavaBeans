e6b02016a7968ab82a0caa2c4b93d4c466391829
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-10-19 11:22:06 +0100

    Implemement create method in delvery employee service

--- src/main/java/org/example/api/DeliveryEmployeeService.java
+++ src/main/java/org/example/api/DeliveryEmployeeService.java
@@ -1,2 +1,17 @@
-package org.example.api;public class DeliveryEmployeeService {
+package org.example.api;
+
+import org.example.cli.DeliveryEmployee;
+import org.example.client.FailedToCreateException;
+import org.example.db.DeliveryEmployeeDAO;
+
+public class DeliveryEmployeeService {
+    private DeliveryEmployeeDAO dao;
+
+    public DeliveryEmployeeService(DeliveryEmployeeDAO dao){
+        this.dao = dao;
+    }
+
+    public int createDeliveryEmployee(DeliveryEmployee deliveryEmployee) throws FailedToCreateException {
+        return dao.createDeliveryEmployee(deliveryEmployee);
+    }
 }
